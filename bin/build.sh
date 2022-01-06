d8e3284d26061a77a525a7ab5db0313e95eef54f
bajorekp <bajorekp@gmail.com>
2022-01-06 13:45:29 +0100

    Open LFS book after last build

--- bin/build.sh
+++ bin/build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
+set -e
+
 cd buildchain
 for script in *.sh
 do
   bash $script > log/$script
 done
+
+open https://www.linuxfromscratch.org/lfs/view/stable-systemd/chapter11/theend.html
